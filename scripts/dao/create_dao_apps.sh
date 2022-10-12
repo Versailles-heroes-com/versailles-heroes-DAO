871b61036f5ec6fcecafead771b3eb726313b04b
spark-ghub <spark@versaillesheroes.com>
2022-10-12 12:17:14 +0800

    add moh-voting support.

--- scripts/dao/create_dao_apps.sh
+++ scripts/dao/create_dao_apps.sh
@@ -5,11 +5,19 @@ source env
 ## create app
 dao install $DAO_ADDRESS agent --env $DEPLOY_ENV
 dao install $DAO_ADDRESS agent --env $DEPLOY_ENV
+dao install $DAO_ADDRESS agent --env $DEPLOY_ENV
 
-# aragon apm info ownership-voting.open.aragonpm.eth --env aragon:rinkeby --ipfs-rpc https://api.pinata.cloud --ipfs-gateway https://gateway.pinata.cloud/ipfs
-dao install $DAO_ADDRESS ownership-voting.open.aragonpm.eth --app-init-args $VOTING_ESCROW 510000000000000000 300000000000000000 86400 2500 10 2500 50000 10 1000 --env $DEPLOY_ENV --ipfs-rpc https://ipfs.infura-ipfs.io:5001/ --ipfs-gateway https://ipfs.infura-ipfs.io/ipfs
+# ownership
+# aragon apm info ownership-voting.open.aragonpm.eth --env aragon:goerli --ipfs-rpc https://api.pinata.cloud --ipfs-gateway https://gateway.pinata.cloud/ipfs
+dao install $DAO_ADDRESS ownership-voting.open.aragonpm.eth --app-init-args $VOTING_ESCROW 510000000000000000 300000000000000000 604800 2500 10 2500 50000 10 1000 --env $DEPLOY_ENV --ipfs-rpc https://ipfs.infura-ipfs.io:5001/ --ipfs-gateway https://ipfs.infura-ipfs.io/ipfs
 
-dao install $DAO_ADDRESS create-guild-voting.open.aragonpm.eth --app-init-args $VOTING_ESCROW 510000000000000000 300000000000000000 86400 2500 1000000 10 2500 50000 1000000 1000000000 10 1000 --env $DEPLOY_ENV --ipfs-rpc https://ipfs.infura-ipfs.io:5001/ --ipfs-gateway https://ipfs.infura-ipfs.io/ipfs
+# moh
+# aragon apm info moh-ownership-voting.open.aragonpm.eth --env aragon:goerli --ipfs-rpc https://api.pinata.cloud --ipfs-gateway https://gateway.pinata.cloud/ipfs
+dao install $DAO_ADDRESS moh-ownership-voting.open.aragonpm.eth --app-init-args $VOTING_ESCROW 510000000000000000 200000000000000000 604800 2500 10 2500 50000 10 1000 --env $DEPLOY_ENV --ipfs-rpc https://ipfs.infura-ipfs.io:5001/ --ipfs-gateway https://ipfs.infura-ipfs.io/ipfs
+
+# createguild
+# aragon apm info create-guild-voting.open.aragonpm.eth --env aragon:goerli --ipfs-rpc https://api.pinata.cloud --ipfs-gateway https://gateway.pinata.cloud/ipfs
+dao install $DAO_ADDRESS create-guild-voting.open.aragonpm.eth --app-init-args $VOTING_ESCROW 510000000000000000 300000000000000000 259200 2500 1000000 10 2500 50000 1000000 1000000000 10 1000 --env $DEPLOY_ENV --ipfs-rpc https://ipfs.infura-ipfs.io:5001/ --ipfs-gateway https://ipfs.infura-ipfs.io/ipfs
 
 dao apps $DAO_ADDRESS --all --env $DEPLOY_ENV
 dao acl view $DAO_ADDRESS --env $DEPLOY_ENV
